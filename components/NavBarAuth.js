6ea3ed17ac3c56a4613692b91c71a3cf1f7ef3f5
Brandon Schnurbusch <b33blbroxx@gmail.com>
2023-10-24 19:41:23 -0500

    Got searchbar working, able to search any member info and get results

--- components/NavBarAuth.js
+++ components/NavBarAuth.js
@@ -5,6 +5,7 @@ import {
   Navbar, Container, Nav, Button,
 } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
+import SearchBar from './SearchBar';
 
 export default function NavBarAuth() {
   return (
@@ -34,6 +35,7 @@ export default function NavBarAuth() {
             </Link>
             <Button variant="danger" onClick={signOut}>Sign Out</Button>
           </Nav>
+          <SearchBar />
         </Navbar.Collapse>
       </Container>
     </Navbar>
