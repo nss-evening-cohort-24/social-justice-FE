7464abd68412824b61645ee91377d76882413fab
Sean Dorris <seanc.dorris@gmail.com>
2023-10-26 16:39:11 -0500

    adjusting components that use the meetup object to match BE instead of Firebase test data

--- components/cards/MeetupCard.js
+++ components/cards/MeetupCard.js
@@ -4,29 +4,39 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Link from 'next/link';
-import { deleteMeetupMembersRelationship } from '../../api/mergedData';
+// import { deleteMeetupMembersRelationship } from '../../api/mergedData';
+import { deleteSingleMeetup } from '../../api/meetupData';
 
 function MeetupCard({ meetupObj, onUpdate }) {
   const deleteMeetup = () => {
     if (window.confirm(`Do you want to delete ${meetupObj.title}?`)) {
-      deleteMeetupMembersRelationship(meetupObj.firebaseKey).then(() => onUpdate());
+      deleteSingleMeetup(meetupObj.id).then(() => onUpdate());
     }
   };
 
+  const meetTime = new Date(meetupObj.meetTime);
+  const formatDate = (date) => {
+    const mm = String(date.getMonth() + 1).padStart(2, '0');
+    const dd = String(date.getDate()).padStart(2, '0');
+    const yyyy = date.getFullYear();
+    return `${mm}-${dd}-${yyyy}`;
+  };
+  const formattedMeetTime = formatDate(meetTime);
+
   return (
     <Card style={{ width: '18rem', margin: '10px' }}>
-      <Card.Img variant="top" src={meetupObj.image} alt={meetupObj.title} style={{ height: '400px' }} />
+      <Card.Img variant="top" src={meetupObj.imageUrl} alt={meetupObj.title} style={{ height: '400px' }} />
       <Card.Body>
         <Card.Title>{meetupObj.title}</Card.Title>
         <span>{meetupObj.description}</span>
-        <h6> {meetupObj.meetTime}</h6>
+        <h6> {formattedMeetTime}</h6>
         <h6> {meetupObj.location}</h6>
         <h6> {meetupObj.attending}</h6>
         <br />
-        <Link href={`/meetup/${meetupObj.firebaseKey}`} passHref>
+        <Link href={`/meetup/${meetupObj.id}`} passHref>
           <Button variant="primary" className="m-2">VIEW</Button>
         </Link>
-        <Link href={`/meetup/edit/${meetupObj.firebaseKey}`} passHref>
+        <Link href={`/meetup/edit/${meetupObj.id}`} passHref>
           <Button variant="info">EDIT</Button>
         </Link>
         <Button variant="danger" onClick={deleteMeetup} className="m-2">
@@ -42,8 +52,8 @@ MeetupCard.propTypes = {
     title: PropTypes.string,
     description: PropTypes.string,
     meetTime: PropTypes.string,
-    image: PropTypes.string,
-    firebaseKey: PropTypes.string,
+    imageUrl: PropTypes.string,
+    id: PropTypes.number,
     location: PropTypes.string,
     attending: PropTypes.bool,
   }).isRequired,
