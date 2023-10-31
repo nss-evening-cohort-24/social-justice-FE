4262c40502a09a5d83bbf3a636611cb039179540
Sean Dorris <seanc.dorris@gmail.com>
2023-10-30 21:21:09 -0500

    members can now remove themselves from meetups

--- components/buttons/AddMemberToMeetup.js
+++ components/buttons/AddMemberToMeetup.js
@@ -5,12 +5,13 @@ import { addMemberToMeetup } from '../../api/meetupData';
 
 export default function AddMemberToMeetup({ meetupId, memberId }) {
   const router = useRouter();
+
   console.log('testing meetupid from parent', meetupId);
   console.log('testing member from parent', memberId);
 
   const AddMember = () => {
     addMemberToMeetup(meetupId, memberId).then(() => {
-      router.push(`/meetup/${meetupId}`);
+      router.push(`/meetup/${meetupId}`).then(() => window.location.reload());
     });
   };
 
